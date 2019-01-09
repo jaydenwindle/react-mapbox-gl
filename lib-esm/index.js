457cce9dfb30619232db8f5d577b00b98708621f
Jayden Windle <jaydenwindle@gmail.com>
2019-01-09 14:46:28 -0500

    added lib so can be installed from yarn

---
+++ lib-esm/index.js
@@ -0,0 +1,17 @@
+import Map from './map';
+import BaseLayer from './layer';
+import layerMouseTouchEvents from './layer-events-hoc';
+import GeoJSONLayer from './geojson-layer';
+import Feature from './feature';
+import ZoomControl from './zoom-control';
+import Popup from './popup';
+import ScaleControl from './scale-control';
+import Marker from './marker';
+import Source from './source';
+import Cluster from './cluster';
+import RotationControl from './rotation-control';
+import { withMap, MapContext } from './context';
+var Layer = withMap(layerMouseTouchEvents(BaseLayer));
+export { Feature, Layer, GeoJSONLayer, Map, Popup, ZoomControl, ScaleControl, Marker, Source, Cluster, RotationControl, MapContext };
+export default Map;
+//# sourceMappingURL=index.js.map
